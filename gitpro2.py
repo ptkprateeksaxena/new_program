e91097c0b625623d807c00cf0748bbc643409cf3
prateek-saxena <ptksna1@gmail.com>
2023-10-20 15:47:52 +0530

    program by raghav

---
+++ gitpro2.py
@@ -0,0 +1,14 @@
+print("Prateek saxena")
+print("this is my second repo program")
+
+user_input = int(input("Enter your age"))
+
+def voter(x):
+    if x <= 18:
+        print("you are not eligable")
+    else:
+        print("you are eligable")
+
+voter(user_input)
+print("prateek")
+print("saxena")
